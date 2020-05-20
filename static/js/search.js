5893d25aa1d9da1cb74f98bf895b6e877d02e71a
Pedro Bastos <hacker@MacBook-Pro.home>
2020-05-20 19:07:09 +0100

    botao home

--- static/js/search.js
+++ static/js/search.js
@@ -11,15 +11,13 @@ $("#ilhavo").click(function(){
 });
 
 $("#search_button").click(function(){
-
     window.location.href = 'results_bylocation.html?local=' + $("#dropdownMenuButton").text() +  $("#dropdownMenuButton1").text()
-
-
 });
 
 $("#search_button1").click(function(){
-    
     window.location.href = 'results_byname.html?name=' + $("#input_text").val()
+});
 
-
+$("#homeButton").click(function(){
+    window.location.href = 'index.html'
 });
